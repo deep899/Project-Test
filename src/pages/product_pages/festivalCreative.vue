8b2180c5a60638988377ab386c68c77fcc3d4478
LAPTOP-QF8J1IKG\omche <omchevli8484@gmail.com>
2022-12-14 11:37:01 +0530

    changesinCerasol_14-12-2022

--- src/pages/product_pages/festivalCreative.vue
+++ src/pages/product_pages/festivalCreative.vue
@@ -455,7 +455,8 @@ export default {
       ],
       slide: ref("style"),
       lorem:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.",
+        "We work  with a variety of industries and global corporations, which is why our Portfolio consists of social media apps, digital marketing sites, and more! As a professional and dynamic company, we aim to provide excellent service to all our clients. A recent project we completed is a Digital Marketing website that contains everything a business website often longs for!Redesigning a website or creating your own app for your product everything will be done personalized. We value honesty, impartiality, and candor, with our partners, and our valuable customers. Life is too short to bother; Let’s have fun and learn. We do our best to be proud of our work!",
+        
     };
   },
   methods: {
