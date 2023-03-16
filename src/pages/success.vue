91aa3858ae45beedc71a1552990b8220311630d6
LAPTOP-QF8J1IKG\omche <omchevli8484@gmail.com>
2023-03-16 11:44:27 +0530

    Revert "ok.."
    This reverts commit ea0ef75ba35d45abb96d7008286447ed7fdcfcae.

--- src/pages/success.vue
+++ src/pages/success.vue
@@ -19,11 +19,6 @@
               font-weight: 400;
 
               line-height: 3rem;
-
-
-
-
-              
               letter-spacing: -0.01562em;
               /* border: 1px solid black !important; */
               color: #00a00b!important;"
