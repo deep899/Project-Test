5388dbd1f3ef8a613cadd748bef2c09fd54fa649
Sonuredjinni <76870599+Sonuredjinni@users.noreply.github.com>
2023-04-06 14:41:00 +0530

    changes

--- src/components/carrertable.vue
+++ src/components/carrertable.vue
@@ -40,7 +40,7 @@ export default {
   setup() {},
   async mounted() {
     let result = await axios.get(
-      "https://uatapi.infinitybrains.com/public/api/showvacancy"
+      "https://api.infinitybrains.com/public/api/showvacancy"
     );
     console.warn(result.data.data);
     this.list = result.data.data;
